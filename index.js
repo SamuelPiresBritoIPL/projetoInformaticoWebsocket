a6b10c90b3776c3afc040b9e5ac5bc6b50cefee9
Daniel Sarreira <2191740@my.ipleiria.pt>
2022-05-26 21:13:36 +0100

    Setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const httpServer = require('http').createServer()
+const io = require("socket.io")(httpServer, {
+    allowEIO3: true,
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+})
+httpServer.listen(8080, function () {
+    console.log('listening on *:8080')
+})
+io.on('connection', function (socket) {
+    console.log(`client ${socket.id} has connected`)
+})
