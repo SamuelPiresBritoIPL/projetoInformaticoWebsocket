cf0f3c5d39e632af49099129083663007dab5b36
Samuel Pires Brito <2200653@my.ipleiria.pt>
2023-04-02 17:22:43 +0100

    Added VSC Tasks

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const httpServer = require('http').createServer()
+const httpServer = require("http").createServer();
 const port = 8080;
 const io = require("socket.io")(httpServer, {
-    allowEIO3: true,
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-        credentials: true
-    }
-})
+	allowEIO3: true,
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"],
+		credentials: true,
+	},
+});
 httpServer.listen(port, function () {
-    console.log('listening on *:' + port)
-})
-io.on('connection', function (socket) {
-    console.log(`client ${socket.id} has connected`)
-})
+	console.log("Listening on port: " + port);
+});
+io.on("connection", function (socket) {
+	console.log(`client ${socket.id} has connected`);
+});
 
-io.on('connection', function (socket) {
-    socket.on('newInscricao', function (response) {
-        socket.broadcast.emit('newInscricao', response)
-    })
-})
+io.on("connection", function (socket) {
+	socket.on("newInscricao", function (response) {
+		socket.broadcast.emit("newInscricao", response);
+	});
+});
