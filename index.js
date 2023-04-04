9b95c9a3a7148ab81afdf078718ff04d860a550d
Samuel Pires Brito <2200653@my.ipleiria.pt>
2023-04-04 17:31:26 +0100

    Information separation and server information added.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const httpServer = require("http").createServer();
+
+//Localmente
 const port = 8080;
+const originIP = "http://localhost:";
+//Old Version
+// const originPort = 3000;
+const originPort = 5173;
+
+/*
+//Servidor
+const port = 8081;
+const originIP = "http://localhost:";
+const originPort = 80;
+*/
 const io = require("socket.io")(httpServer, {
 	allowEIO3: true,
 	cors: {
-		origin: "http://localhost:3000",
+		origin: originIP + originPort,
 		methods: ["GET", "POST"],
 		credentials: true,
 	},
