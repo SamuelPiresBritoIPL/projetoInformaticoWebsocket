c583c55adcd83aea050fc97db7c9fa72507314d2
Carolina Antunes <carolinapazeiro@gmail.com>
2023-04-12 10:55:33 +0100

    added my ip

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const httpServer = require("http").createServer();
 
 //Localmente
 const port = 8080;
-const originIP = "http://localhost:";
+//const originIP = "http://localhost:";
+//for Carolina
+const originIP = "http://127.0.0.1:";
 //Old Version
 // const originPort = 3000;
 const originPort = 5173;
@@ -14,22 +16,22 @@ const originIP = "http://localhost:";
 const originPort = 80;
 */
 const io = require("socket.io")(httpServer, {
-	allowEIO3: true,
-	cors: {
-		origin: originIP + originPort,
-		methods: ["GET", "POST"],
-		credentials: true,
-	},
+  allowEIO3: true,
+  cors: {
+    origin: originIP + originPort,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 httpServer.listen(port, function () {
-	console.log("Listening on port: " + port);
+  console.log("Listening on port: " + port);
 });
 io.on("connection", function (socket) {
-	console.log(`client ${socket.id} has connected`);
+  console.log(`client ${socket.id} has connected`);
 });
 
 io.on("connection", function (socket) {
-	socket.on("newInscricao", function (response) {
-		socket.broadcast.emit("newInscricao", response);
-	});
+  socket.on("newInscricao", function (response) {
+    socket.broadcast.emit("newInscricao", response);
+  });
 });
